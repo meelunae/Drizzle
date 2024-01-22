90b6739bd1313d222a2e724a0d5153290c3e0e70
meelunae <git@eleuna.me>
2024-01-22 18:03:01 +0100

    Initial Commit

---
+++ Drizzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drizzle
+//
+//  Created by Emanuele Iaccarino on 22/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drizzle/DrizzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrizzleApp.swift
+//  Drizzle
+//
+//  Created by Emanuele Iaccarino on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DrizzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
