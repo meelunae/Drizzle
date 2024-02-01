8e79ee2314f3e749a9022d58462f431394ec8fd3
meelunae <git@eleuna.me>
2024-02-01 23:49:22 +0100

    Edited logos for menu bar and app

---
+++ Drizzle/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Drizzle
+//
+//  Created by Emanuele Iaccarino on 01/02/24.
+//
+
+import Foundation
---
+++ Drizzle/Views/CloudView.swift
@@ -0,0 +1,8 @@
+//
+//  CloudView.swift
+//  Drizzle
+//
+//  Created by Emanuele Iaccarino on 01/02/24.
+//
+
+import Foundation
